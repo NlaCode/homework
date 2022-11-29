baf2e986b6a318d45db1479664fa0d7420f2dcb6
André Nascimento <andre.nascimento@grupomateus.com>
2022-11-28 21:31:56 -0300

    first commit

---
+++ cupons.sql
@@ -0,0 +1,30 @@
+SELECT
+cupom.id_filial,
+cupom.codigo_pdv,
+date_format(cupom.data_cupom, '01/MM/yyyy') AS data_mes,
+produto.id_departamento,
+CASE
+WHEN (produto.id_departamento IN (25,37,40,41,42,43,44,45,46,47,53) OR produto.id_secao IN (1318,1331,1337,1475))
+THEN 2  ELSE   1 END    id_loja_emporio,
+CASE
+WHEN (produto.id_departamento IN (25,37,40,41,42,43,44,45,46,47,53) OR produto.id_secao IN (1318,1331,1337,1475))
+THEN 'EMPORIO' ELSE 'LOJA' END        AS loja_emporio,
+COUNT(DISTINCT cupom.id_cupom)        as qtd_cupons,
+SUM(cupom.preco_total_item - cupom.valor_desconto_item  + cupom.valor_acrescimo_item) AS total_faturado,
+COUNT(DISTINCT cupom.quantidade_item) as qtd_item
+FROM bi_vendas_gold.f_cupom_item as cupom 
+LEFT JOIN bi_vendas_gold.dim_filial as filial on filial.id = cupom.id_filial
+LEFT JOIN bi_vendas_gold.dim_produto as produto on produto.ID = cupom.id_produto
+WHERE cupom.data_cupom BETWEEN date_trunc('year', current_date) - (interval '1 year') AND current_date - (interval '1 day')
+
+
+
+and filial.id_empresa IN (2, 15)
+and cupom.situacao_cupom = 1
+and cupom.situcao_item = 0
+GROUP by
+date_format(cupom.data_cupom, '01/MM/yyyy'),
+cupom.id_filial,
+cupom.codigo_pdv,
+produto.id_departamento,
+produto.id_secao
